793b11d138ce89bdc41871c4e1ab458e82316829
HsiaoCz <984274788@qq.com>
2023-04-08 10:11:03 +0800

    python-master

---
+++ 04define-class/02xulie-abc/xulie-abc.py
@@ -0,0 +1,15 @@
+# 序列中的abc继承关系
+# abc抽象基类
+# 抽象基类有点类似于接口
+# 在一个基础类里面实现一些方法，所有继承这个类的子类需要覆盖这些方法
+
+# 一些python实现的抽象基类放在collections_abc这个模块里
+# 其中和序列相关的sequence代表不可变序列的基类
+# mutableSquence代表可变序列的基类
+
+# 可变序列可变体现在
+# MutableSequence里由__setitem__和delitem__两个魔法函数
+# 可以设置值和删除值
+
+
+
